cbec215ffdc55cfe8aa46cd1b6b5b13d628460c2
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-19 17:21:55 -0500

    Update app.js

--- Curso JS Moderno/01-Introduccion/js/app.js
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -1,4 +1,9 @@
+// Pregunta al usuario su nombre en pantalla.
 const nombre = prompt("cual es tu nombre?");
+//Toma el nombre y lo muestra en pantalla.
 document.querySelector(
   ".contenido"
 ).innerHTML = `${nombre} esta aprendiendo javaScript moderno`;
+
+//asigna el valor hacia la variable producto.
+const producto = "Monitor 24 pulgadas";
