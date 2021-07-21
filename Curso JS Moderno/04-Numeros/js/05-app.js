a98c542e186e491c54387098a58072627ad90cc0
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-21 13:54:13 -0500

    JavaScript
    convertir strings a numeros enteros

---
+++ Curso JS Moderno/04-Numeros/js/05-app.js
@@ -0,0 +1,10 @@
+let puntaje = 5;
+
+puntaje -= 3;
+
+puntaje--; //Primero se aigna el degrecmento ala variable pero hasta
+//que se le hace otro llamado se le da el decremento
+
+--puntaje; //desde el primer instante que se le hace un llamado al variable se le hace el decremento
+
+console.log(puntaje);
---
+++ Curso JS Moderno/04-Numeros/js/06-app.js
@@ -0,0 +1,13 @@
+const numero1 = "20";
+const numero2 = "20";
+const numero3 = "uno";
+const numero4 = 20;
+
+console.log(numero1);
+
+console.log(Number.parseInt(numero1));
+console.log(Number.parseFloat(numero2));
+
+console.log(Number.parseInt(numero3));
+
+console.log(Number.isInteger(numero4));
