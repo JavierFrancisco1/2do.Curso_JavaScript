a81bc31f1b0e64c6f1bce20cfcdc22b24c2475f8
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-21 11:22:49 -0500

    Update 01-app.js
    crear numeros en javaScript

---
+++ Curso JS Moderno/04-Numeros/js/01-app.js
@@ -0,0 +1,15 @@
+//creacion de numeros
+
+const numero1 = 30;
+const numero2 = 20;
+const nuemro3 = 20.2;
+const numero4 = 0.10203;
+const numero5 = -20;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero4);
+console.log(numero5);
+
+const numero6 = new Number(80);
+console.log(numero6);
