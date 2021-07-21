46c6afb85122227ff36743befd455d9e7b5fccdc
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-21 09:27:20 -0500

    JavaScript
    includes y length

---
+++ Curso JS Moderno/03-Strings/js/02-app.js
@@ -0,0 +1,10 @@
+const producto = "Monitor de 20 pulgadas";
+
+//Conocer la cantidad de letras de la cadena de texto mencionada con .length
+console.log(producto.length);
+
+console.log(producto.indexOf("tablet"));
+
+console.log(producto.includes("tablet"));
+console.log(producto.includes("Monitor"));
+console.log(producto.includes("monitor"));
