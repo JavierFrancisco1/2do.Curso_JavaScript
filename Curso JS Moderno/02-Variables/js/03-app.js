3b8a64ae4483b50aad2e1dd719807950c96b420c
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-21 09:21:05 -0500

    JavaScript
    crear strings en JavaScript

---
+++ Curso JS Moderno/02-Variables/js/03-app.js
@@ -0,0 +1,11 @@
+const producto = "Tablet";
+
+//No se pueden reasignar
+// producto = "Monitor";
+
+console.log(producto);
+
+
+const precio;
+precio = 20;
+console.log(precio);
---
+++ Curso JS Moderno/03-Strings/js/01-app.js
@@ -0,0 +1,6 @@
+const producto = "Monitor de 20 pulgadas";
+const producto2 = String("monitor 24pulgadas");
+const producto3 = new String("monitor de 27 pulgadas");
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
