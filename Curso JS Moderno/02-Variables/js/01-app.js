cf5e9e73403aff111c3532884f80360b2eaded62
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-21 09:04:09 -0500

    Update 01-app.js
    variables con var

--- Curso JS Moderno/02-Variables/js/01-app.js
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -1,2 +1,36 @@
-var producto = 'Monitor de 24 pulgadas';
+//inicializar una variable con valor.
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
 
+//Las variables se pueden resasignar
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+
+//JAvaSCript es un lenguaje de tipo dinamico,no se especifica tipo de dato.
+producto = 15;
+console.log(producto);
+
+//Se pueden inicializar sin valor y asignarlo despues.
+
+var disponible;
+disponible = true;
+
+disponible = false;
+
+//inicializar multiples variables.
+
+var categoria = "computadora",
+  marca = "Marca famosa",
+  calificacion = 5;
+
+
+  //Las variables no pueden iniciar con numeros
+  var 99Dias;
+  var Dias99;
+
+  var 01;
+  var_01;
+
+  var nombreProductoCategoriaPrecio;
+  var nombre_producto_categoria_precio;
+  var Producto;
