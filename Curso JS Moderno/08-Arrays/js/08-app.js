f6ccc2f6c9d8b24d5bcfc69a6e726e344e3249d8
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-22 14:30:20 -0500

    js
    destructuring en arrays

---
+++ Curso JS Moderno/08-Arrays/js/08-app.js
@@ -0,0 +1,23 @@
+const producto = {
+  nombre: "Monitor 20 pulgadas",
+  precio: 300,
+  disponible: true,
+};
+
+//   const nombre = producto.nombre;
+//   console.log(producto.nombre);
+
+//Destructuring
+const { nombre } = producto;
+
+console.log(nombre);
+
+
+//Destructuring con arreglos
+
+const numeros = [10,20,30,40,50];
+
+const [primero,segundo,...tercero] = numeros;
+console.log(tercero);
+
+
