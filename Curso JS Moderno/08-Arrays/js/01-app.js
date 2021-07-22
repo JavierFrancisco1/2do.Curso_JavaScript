4423d077f88dddbd261170c948cef875e3e5fde1
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-22 13:42:05 -0500

    js
    acceder alos valores de un arreglo

---
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,14 @@
+const numeros = [10];
+
+const meses = new Array('enero','Febrero','Marzo');
+
+console.log(numeros);
+console.log(meses);
+
+
+
+// Un arreglo que contiene datos de todo tipo
+
+
+const deTodo = ['Hola',10,true,'si',false,{nombre: 'juan',trabajo: 'programador'}, [1,2,3]];
+console.log(deTodo);
