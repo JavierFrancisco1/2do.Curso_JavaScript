81f20ccca039252f485d5955ae33547a051ba335
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-22 09:17:29 -0500

    Js
    buenas practicas ala hora de evaluar un boolean

---
+++ Curso JS Moderno/06-Booleans/js/03-app.js
@@ -0,0 +1,11 @@
+const autenticado = true;
+
+if (autenticado) {
+  console.log("si puedes ver netflix");
+} else {
+  console.log("No no puedes ver netflix");
+}
+
+// Operador ternario
+
+console.log(autenticado ? "si esta autenticado" : "no esta autenticado");
