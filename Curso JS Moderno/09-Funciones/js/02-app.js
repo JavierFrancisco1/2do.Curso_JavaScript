caccb1d9e20f35eeb232028be314099c0b69a37c
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-22 18:47:07 -0500

    js
    funciones nativas

---
+++ Curso JS Moderno/09-Funciones/js/02-app.js
@@ -0,0 +1,16 @@
+// Declaracion de funcion
+
+sumar();
+function sumar() {
+  console.log(2 + 2);
+}
+
+
+//Expresion de funcion
+
+const sumar2 = function () {
+  console.log(3 + 3);
+};
+
+sumar2();
+
