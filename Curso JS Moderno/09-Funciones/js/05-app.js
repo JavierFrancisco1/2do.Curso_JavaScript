440139f5b69533b326fc308019a7f21897e88453
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-22 18:59:42 -0500

    js
    parametros por default

---
+++ Curso JS Moderno/09-Funciones/js/05-app.js
@@ -0,0 +1,16 @@
+function sumar(a, b) {
+  // a,b son parametros.
+  console.log(a + b);
+}
+
+sumar(3, 6); // 2- 3 son argumentos
+
+sumar(200, 184);
+sumar(124, 321);
+
+function saludar(nombre, apellido) {
+  console.log(`Hola: ${nombre} ${apellido}`);
+}
+
+saludar('juan','de la torre')
+saludar()
