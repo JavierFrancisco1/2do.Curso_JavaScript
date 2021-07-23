15160063fc1ecca6b7b609aa8de73194fac01e3a
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-22 20:43:27 -0500

    Js
    Arrow functions en el reproductor de musica

---
+++ Curso JS Moderno/09-Funciones/js/13-app.js
@@ -0,0 +1,29 @@
+const reproductor = {
+  cancion: "",
+  reproducir: (id) => console.log(`Reproduciendo cancion con el id ${id}`),
+  pausar: () => console.log("Pausando..."),
+  borrar: (id) => console.log(`Borrando cancion.. ${id}`),
+  crearPlayList: (nombre) =>
+    console.log(`Agregando una nueva playList: ${nombre}`),
+  reproducirPlayList: (nombre) => console.log(`reproduciendo: ${nombre}`),
+
+  set nuevaCancion(cancion) {
+    this.cancion = cancion;
+    console.log(`añadiendo: ${cancion}`);
+  },
+
+  get obtenerCancion() {
+    console.log(`${this.cancion}`);
+  },
+};
+
+reproductor.nuevaCancion = "enter sadman";
+reproductor.obtenerCancion;
+
+reproductor.reproducir(30);
+reproductor.reproducir(20);
+reproductor.pausar();
+reproductor.borrar(30);
+reproductor.crearPlayList("heavy metal");
+reproductor.crearPlayList("Rock 90s");
+reproductor.reproducirPlayList("heavy Metal");
