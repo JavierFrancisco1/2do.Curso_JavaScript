ec43da1f4eb048fb1f4e228a4b7cb85bf03d0dd6
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-24 09:39:38 -0500

    Js
    operador ternario

---
+++ Curso JS Moderno/10-Estructuras/js/09-app.js
@@ -0,0 +1,11 @@
+const autenticado = false;
+
+const puedePagar = false;
+
+console.log(
+  autenticado
+    ? puedePagar
+      ? "si esta autenticado y puede pagar"
+      : "si esta autenticado, no puede pagar"
+    : "No no esta autenticado"
+);
