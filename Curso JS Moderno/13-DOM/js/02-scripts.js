c9a792176e25550336ab109de67652fee2916b6f
JavierFrancisco1 <javierfrancisco2212@gmail.com>
2021-07-24 15:29:58 -0500

    getElementByClassName
    getElementByClassName como seleccionar uno o varias clases dentro del documento

---
+++ Curso JS Moderno/13-DOM/js/02-scripts.js
@@ -0,0 +1,17 @@
+//seleccionar elementos por su clase
+
+const header = document.getElementsByClassName("header");
+console.log(header);
+
+const hero = document.getElementsByClassName("hero");
+console.log(hero);
+
+/// si las clases exiten mas de 1 vez
+
+const contenedores = document.getElementsByClassName("contenedor");
+console.log(contenedores);
+
+// si una clase no existe
+
+const noExiste = document.getElementsByClassName("no-existe");
+console.log(noExiste);
