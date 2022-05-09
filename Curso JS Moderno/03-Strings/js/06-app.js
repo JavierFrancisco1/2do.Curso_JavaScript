42388d076a99f9565c8bbb2309c390e69263da86
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-09 14:04:20 -0500

    repeat y split

--- Curso JS Moderno/03-Strings/js/06-app.js
+++ Curso JS Moderno/03-Strings/js/06-app.js
@@ -1,20 +1,20 @@
-const producto = "Monitor de 20 pulgadas";
+const producto = "Monitor de 20 pulgadas ";
 
 //.Repeat te va permitir una cadena de texto.
 const texto = " en promocion".repeat(2.4);
 
 console.log(texto);
+console.log(`${producto} ${texto} !!!`);
 
-console.log(`${producto} ${texto}!!!`);
 
-//split, dividir un string
+// split, dividir un string
 
-const actividad = "Estoy aprendiendo JavaScript Moderno";
+const actividad = 'Estoy aprendiendo javaScript moderno';
+console.log(actividad.split(' '));
 
-console.log(actividad.split(" "));
+const hobbies = 'Leer,caminar,escuchar música,escribir,aprender a programar';
+console.log(hobbies.split(','));
 
-const hobbies = "Leer caminar, biblioteca,escribir,aprender a programar";
-console.log(hobbies.split(", "));
+const tweet = 'Aprendiendo javaScript #JSModernoConJuan';
+console.log(tweet.split('#'));
 
-const tweet = "apendiendo JavaScript #JSModernoConJuan";
-console.log(tweet.split("#"));
