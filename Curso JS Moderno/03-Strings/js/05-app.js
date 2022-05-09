844266b7344bb3f6eb07247e18a075c4304957f9
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-09 13:57:26 -0500

    string methods replace,slice y substring

--- Curso JS Moderno/03-Strings/js/05-app.js
+++ Curso JS Moderno/03-Strings/js/05-app.js
@@ -3,20 +3,19 @@ const producto = "Monitor de 20 pulgadas";
 // .replace para reemplazar
 console.log(producto);
 console.log(producto.replace("pulgadas", '"'));
+console.log(producto.replace("Monitor", "monitor curvo"));
 
-console.log(producto.replace("Monitor", "Monitor curvo"));
-
-console.log();
-
-/// .slice para cortar
-
+//.slice para cortar
 console.log(producto.slice(0, 10));
-// console.log(producto.slice(2, 1));
+//console.log(producto.slice(8));
+console.log(producto.slice(2, 1));
 
 // Alternativa a slice
 
 console.log(producto.substring(0, 10));
+console.log(producto.substring(2, 1));
 
-const usuario = "Juan";
+const usuario = "juan";
+
 console.log(usuario.substring(0, 1));
 console.log(usuario.charAt(0));
