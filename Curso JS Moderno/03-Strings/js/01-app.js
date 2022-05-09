f60eeab3225e0dfac839128d6ee4d85118c684ec
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-09 13:34:58 -0500

    crear Strings

--- Curso JS Moderno/03-Strings/js/01-app.js
+++ Curso JS Moderno/03-Strings/js/01-app.js
@@ -1,6 +1,7 @@
-const producto = "Monitor de 20 pulgadas";
-const producto2 = String("monitor 24pulgadas");
-const producto3 = new String("monitor de 27 pulgadas");
+const producto = "Monitor 20\"";
+const producto2 = String("Monitor 24'");
+
+const producto3 = new String('Monitor de 27 pulgadas')
 console.log(producto);
 console.log(producto2);
 console.log(producto3);
