c21e702c00fcbcea038a6bbb846335a8ac329302
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-10 09:36:20 -0500

    comparar 2 valores

--- Curso JS Moderno/05-Operadores/js/02-app.js
+++ Curso JS Moderno/05-Operadores/js/02-app.js
@@ -7,17 +7,15 @@ const numero3 = 30;
 console.log(numero1 == numero3);
 console.log(numero1 == numero2);
 
-//comparador estricto
-
+//Comparador estricto
 console.log(numero1 === numero2);
 console.log(numero1 === parseInt(numero2));
-console.log(typeof numero1);
-console.log(typeof numero2);
+console.log(typeof numero1 );
+console.log(typeof numero2 );
 
-//Diferente
-
-const password1 = "admin";
-const password2 = "admin";
+// Diferente
+const password1 = 'admin';
+const password2 = 'Admin';
 
 console.log(password1 != password2);
 console.log(numero1 != numero2);
