f9d9f13a9b0215ef2467f914ac14fcf95476f6cd
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-10 12:15:36 -0500

    Funciones en objetos
    como acceder alos valores del mismo objeto para utilizarlos en un metodo dentro de ese objeto

--- Curso JS Moderno/07-Objetos/js/11-app.js
+++ Curso JS Moderno/07-Objetos/js/11-app.js
@@ -3,19 +3,21 @@ const producto = {
   precio: 300,
   disponible: true,
   mostrarInfo: function () {
-   return console.log(`El producto: ${this.nombre} tiene un precio de: $${this.precio}`);
+    console.log(
+      `El producto: ${this.nombre} Tiene un precio de ${this.precio}`
+    );
+  },
+};
+const producto2= {
+  nombre: "Tablet",
+  precio: 3000,
+  disponible: true,
+  mostrarInfo: function () {
+    console.log(
+      `El producto: ${this.nombre} Tiene un precio de ${this.precio}`
+    );
   },
 };
 
-const producto2 = {
-    nombre: "Tablet",
-    precio: 3000,
-    disponible: true,
-    mostrarInfo: function () {
-     return console.log(`El producto: ${this.nombre} tiene un precio de: $${this.precio}`);
-    },
-  };
-
 producto.mostrarInfo();
-
 producto2.mostrarInfo();
