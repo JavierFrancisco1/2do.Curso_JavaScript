77bb501f0191e5278edf91252af1f9884fb574b1
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-10 10:14:48 -0500

    Crear objetos

--- Curso JS Moderno/07-Objetos/js/01-app.js
+++ Curso JS Moderno/07-Objetos/js/01-app.js
@@ -1,14 +1,12 @@
 const nombre = "Monitor 20 pulgadas";
 const precio = 300;
-const disponile = true;
+const disponible = true;
 
-// un objeto agrupa todo en una sola variable...
+// Un objeto agrupa todo en una sola variable
 
-//Object literal
 const producto = {
-  nombre: "Monitor 20 pulgadas",
-  precio: 300,
-  disponible: true,
+  nombre,
+  precio,
+  disponible,
 };
-
 console.log(producto);
