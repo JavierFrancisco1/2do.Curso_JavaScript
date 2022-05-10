20ca49c4199dc0d72bf0ab9a9edac6a3804f1cae
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-10 12:26:35 -0500

    Object:keys,values,entries

--- Curso JS Moderno/07-Objetos/js/13-app.js
+++ Curso JS Moderno/07-Objetos/js/13-app.js
@@ -3,9 +3,11 @@ const producto = {
   precio: 300,
   disponible: true,
 };
-
+// Keys nos devuelve las llaves del objeto
 console.log(Object.keys(producto));
 
+//Values nos devuelve el valor de las llaves o propiedades del objeto del objeto
 console.log(Object.values(producto));
 
+//nos devuelve en pares la llave y el valor de cada propiedad de nuestro objeto
 console.log(Object.entries(producto));
