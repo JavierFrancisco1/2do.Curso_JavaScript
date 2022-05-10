646929eea74965ed7dcc90bd02bec72e574f7b78
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-10 12:36:08 -0500

    crear Arrays

--- Curso JS Moderno/08-Arrays/js/01-app.js
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -1,14 +1,20 @@
 const numeros = [10];
 
-const meses = new Array('enero','Febrero','Marzo');
+const meses = new Array("Enero", "Febrero", "Marzo");
 
 console.log(numeros);
 console.log(meses);
 
-
-
 // Un arreglo que contiene datos de todo tipo
 
+const deTodo = [
+  "Hola",
+  10,
+  true,
+  "si",
+  null,
+  { nombre: "Javier", trabajo: "programador" },
+  [1,2,3]
+];
 
-const deTodo = ['Hola',10,true,'si',false,{nombre: 'juan',trabajo: 'programador'}, [1,2,3]];
 console.log(deTodo);
