cc4ad9b9a795b17e9c59d4bf0e77c40eb4e3b035
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-10 12:04:27 -0500

    Sellar un objeto

--- Curso JS Moderno/07-Objetos/js/09-app.js
+++ Curso JS Moderno/07-Objetos/js/09-app.js
@@ -6,14 +6,16 @@ const producto = {
   disponible: true,
 };
 
-Object.seal(producto)
+Object.seal(producto);
 
 producto.disponible = false;
-// producto.imagen = "imagen.jpg";
+//  producto.imagen = "imagen.jpg";
+//  delete producto.precio;
 
 console.log(producto);
 
 console.log(Object.isSealed(producto));
 
-
-
+/* con el metodo (seal) no se puede eliminar ni agregar otra propiedad al objeto 
+ solo se puede modificar el valor de las propiedades del objeto como el boolean ..como arriba le cambiamos
+ de true a false */
