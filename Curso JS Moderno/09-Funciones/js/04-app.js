69e384d16708fa290aeb65c74bc37d483cf2f534
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-11 09:18:14 -0500

    Diferencia entre una funcion y un metodo

--- Curso JS Moderno/09-Funciones/js/04-app.js
+++ Curso JS Moderno/09-Funciones/js/04-app.js
@@ -1,12 +1,13 @@
 const numero1 = 20;
 const numero2 = "20";
 
-console.log(parseInt(numero2));
+console.log(parseInt(numero2)); // Esto es una función
 
-console.log(numero1.toString());
+console.log(numero1.toString()); // Esto es un método
 
-function sumar(){
-    console.log(2 + 2);
-}
 
-sumar();
+function sumar(numero1, numero2) {
+    return console.log(numero1 + numero2);
+  }
+  
+  sumar(5, 10);
