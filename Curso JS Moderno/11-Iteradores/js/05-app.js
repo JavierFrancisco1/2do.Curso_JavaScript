d4d42336a7f81365f907c5ec32961af1ac9b9902
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-12 13:16:38 -0500

    do while

--- Curso JS Moderno/11-Iteradores/js/05-app.js
+++ Curso JS Moderno/11-Iteradores/js/05-app.js
@@ -1,13 +1,6 @@
-let i = 1; //inicio
+let i = 1000;
 
-do{
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log(`${i} FIZZBUZZ`);
-      } else if (i % 3 === 0) {
-        console.log(`${i} Fizz`);
-      } else if (i % 5 === 0) {
-        console.log(`${i} Buzz`);
-      }
-
-    i++; //incremento
-}while(i <= 100) //condicion
+do {
+  console.log(`numero: ${i}`);
+  i++; // Incremento
+} while (i < 10); // condicion
