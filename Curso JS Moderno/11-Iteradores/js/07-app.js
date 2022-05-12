5683db3e5adb1da956ee8029bccd028e3b41a2a3
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-12 15:35:02 -0500

    for in con Object.entries

--- Curso JS Moderno/11-Iteradores/js/07-app.js
+++ Curso JS Moderno/11-Iteradores/js/07-app.js
@@ -1,18 +1,18 @@
-const pendientes = ["tarea", "comer", "Proyecto", "Estudiar JavaScript"];
+const pendientes = ["tarea", "comer", "proyecto", "estudiar JavaScript"];
 
 const carrito = [
   { nombre: "Monitor 27 pulgadas", precio: 500 },
   { nombre: "Television", precio: 100 },
-  { nombre: "Tablet", precio: 200 },
+  { nombre: "Tablet", precio: 200, descuento: true },
   { nombre: "Audifonos", precio: 300 },
   { nombre: "Teclado", precio: 400 },
   { nombre: "Celular", precio: 700 },
 ];
 
-for (pendiente of pendientes) {
+for (let pendiente of pendientes) {
   console.log(pendiente);
 }
 
-for (producto of carrito) {
+for (let producto of carrito) {
   console.log(producto.nombre);
 }
