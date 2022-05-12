3567ae8a52fe82bc41fa23d687be949614ac44cc
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-12 11:32:03 -0500

    Operador ternario

--- Curso JS Moderno/10-Estructuras/js/09-app.js
+++ Curso JS Moderno/10-Estructuras/js/09-app.js
@@ -1,11 +1,10 @@
 const autenticado = false;
-
 const puedePagar = false;
 
 console.log(
-  autenticado
-    ? puedePagar
-      ? "si esta autenticado y puede pagar"
-      : "si esta autenticado, no puede pagar"
+    autenticado? 
+    puedePagar? 
+    "Si esta autenticado y puede pagar"
+    : "si esta autenticado,pero no puede pagar"
     : "No no esta autenticado"
 );
