fd02e33cdc91ba4e01b8a18ae361bb4e43797201
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-12 13:07:02 -0500

    Ejercicio fizz buzz!

--- Curso JS Moderno/11-Iteradores/js/03-app.js
+++ Curso JS Moderno/11-Iteradores/js/03-app.js
@@ -1,15 +1,16 @@
-// Fizz buz...
+// fizz buzz - 100 números
 
 // 3 6 9 12 ... fizz
-// 5 10 15 20 ... buzzz
-// 15 30 45 .. FIZZBUZZ!
+// 5 10 15 20 ... buzz
+// 15 30 45 fizzbuzz!
 
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log(`${i} FIZZBUZZ`);
-  } else if (i % 3 === 0) {
-    console.log(`${i} Fizz`);
-  } else if (i % 5 === 0) {
-    console.log(`${i} Buzz`);
+  if (i % 3 === 0) {
+    console.log(`${i} FizzBuzz!`);
   }
+  else if (i % 3 === 0) {
+    console.log(`${i} fizz`);
+  } else if (i % 5 === 0) {
+    console.log(`${i} buzz`);
+  } 
 }
