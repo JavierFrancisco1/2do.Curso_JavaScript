9f8ecafe26e8bca69e8ec04c6096f677a6916c38
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-12 12:56:01 -0500

    break y continue

--- Curso JS Moderno/11-Iteradores/js/01-app.js
+++ Curso JS Moderno/11-Iteradores/js/01-app.js
@@ -2,14 +2,13 @@
 //   console.log(`numero: ${i}`);
 // }
 
-// for(let i = 0; i <= 20 ; i++){
-//     if(i %2 === 0 ){
-//         console.log(`el numero: ${i} es par`);
-//     }
-//     else if(i %2 === 1){
-//         console.log(`el numero: ${i} es impar`);
-//     }
-// }
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(`numero ${i} es Par`);
+  } else {
+    console.log(`numero ${i} es Impar`);
+  }
+}
 
 const carrito = [
   { nombre: "Monitor 27 pulgadas", precio: 500 },
@@ -19,6 +18,8 @@ const carrito = [
   { nombre: "Teclado", precio: 400 },
   { nombre: "Celular", precio: 700 },
 ];
+
+console.log(carrito.length);
 
 for (let i = 0; i < carrito.length; i++) {
   console.log(carrito[i].nombre);
