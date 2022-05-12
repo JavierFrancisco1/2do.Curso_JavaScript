a4ff3c72eee1533c729b2b95f87de6074794863e
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-12 13:26:50 -0500

    forEach y .map en arrays

--- Curso JS Moderno/11-Iteradores/js/06-app.js
+++ Curso JS Moderno/11-Iteradores/js/06-app.js
@@ -1,24 +1,23 @@
-//forEach
+// forEach
 
-const pendientes = ["tarea", "comer", "Proyecto", "Estudiar JavaScript"];
+const pendientes = ["tarea", "comer", "proyecto", "estudiar JavaScript"];
 
-pendientes.forEach((pendiente, indice) => {
-  console.log(`${indice}: ${pendiente}`);
+pendientes.forEach((pendiente, index) => {
+  console.log(`pendiente ${index + 1}: ${pendiente}`);
 });
 
 const carrito = [
   { nombre: "Monitor 27 pulgadas", precio: 500 },
   { nombre: "Television", precio: 100 },
-  { nombre: "Tablet", precio: 200 },
+  { nombre: "Tablet", precio: 200, descuento: true },
   { nombre: "Audifonos", precio: 300 },
   { nombre: "Teclado", precio: 400 },
   { nombre: "Celular", precio: 700 },
 ];
 
-const nuevoArreglo = carrito.forEach((producto) => producto.nombre);
+const nuevoArreglo = carrito.forEach((producto) => producto.precio);
 
-const nuevoArreglo2 = carrito.map((producto) => producto.nombre);
+const nuevoArreglo2 = carrito.map((producto) => producto.precio);
 
 console.log(nuevoArreglo);
-
 console.log(nuevoArreglo2);
