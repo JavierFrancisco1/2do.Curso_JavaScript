ba218162630501afb991b3c12d5fcd6e775c271e
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-13 13:34:36 -0500

    Eliminar elementos del DOM

--- Curso JS Moderno/13-DOM/js/09-scripts.js
+++ Curso JS Moderno/13-DOM/js/09-scripts.js
@@ -1,10 +1,11 @@
 // const primerEnlace = document.querySelector("a");
 // primerEnlace.remove();
 
-//Eliminar desde el padre
+//Eliminar desde el padre...
 
 const navegacion = document.querySelector(".navegacion");
 
 console.log(navegacion.children);
 
-navegacion.removeChild(navegacion.children[2]);
+navegacion.removeChild(navegacion.children[2])
+console.log(navegacion.children);
