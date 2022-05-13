a5b6bd160c3f21e4ecd1e2a48b97e715c35168fc
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-12 21:31:50 -0500

    modificar textos o imagenes con js

--- Curso JS Moderno/13-DOM/js/06-scripts.js
+++ Curso JS Moderno/13-DOM/js/06-scripts.js
@@ -1,13 +1,10 @@
 const encabezado = document.querySelector(".contenido-hero h1");
 console.log(encabezado);
 
-// console.log(encabezado.innerText); // si en el css - visibility: hidden no lo va a encontrar
-// console.log(encabezado.textContent); // si lo va a encontrar
-// console.log(encabezado.innerHTML); // se trae el html
+// console.log(encabezado.textContent); // Si en el Css - visibility: hidden; no lo va a encontrar
+// console.log(encabezado.innerText); // si lo va a encontrar
+// console.log(encabezado.innerHTML); // se trae el HTML
 
 // const nuevoHeading = "Nuevo Heading";
-
 // document.querySelector(".contenido-hero h1").textContent = nuevoHeading;
 
-const imagen = document.querySelector(".card img");
-imagen.src = "img/hacer2.jpg";
