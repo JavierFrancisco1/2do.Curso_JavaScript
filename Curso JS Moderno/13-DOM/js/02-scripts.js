7a3975b33cd07b1ef691fae215a6feb0c2300ba0
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-12 20:36:26 -0500

    getElementByClassName

--- Curso JS Moderno/13-DOM/js/02-scripts.js
+++ Curso JS Moderno/13-DOM/js/02-scripts.js
@@ -1,4 +1,4 @@
-//seleccionar elementos por su clase
+// seleccionar elementos por su clase...
 
 const header = document.getElementsByClassName("header");
 console.log(header);
@@ -6,12 +6,12 @@ console.log(header);
 const hero = document.getElementsByClassName("hero");
 console.log(hero);
 
-/// si las clases exiten mas de 1 vez
+// si las clases existen mas de una vez
 
 const contenedores = document.getElementsByClassName("contenedor");
 console.log(contenedores);
 
 // si una clase no existe
 
-const noExiste = document.getElementsByClassName("no-existe");
+const noExiste = document.getElementsByClassName("noExiste");
 console.log(noExiste);
