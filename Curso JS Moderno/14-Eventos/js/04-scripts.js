519f719d2c0f416b6f243103a377d711ccc90bc2
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-13 19:00:00 -0500

    evento submit en un formulario

--- Curso JS Moderno/14-Eventos/js/04-scripts.js
+++ Curso JS Moderno/14-Eventos/js/04-scripts.js
@@ -1,11 +1,12 @@
 const formulario = document.querySelector("#formulario");
+console.log(formulario);
 
-formulario.addEventListener("submit", validarFormulario);
+formulario.addEventListener("submit", validarformulario);
 
-function validarFormulario(e) {
+function validarformulario(e){
   e.preventDefault();
 
-  console.log("Consultar una api");
+  console.log("Consultar una api...");
 
   console.log(e.target.action);
 }
