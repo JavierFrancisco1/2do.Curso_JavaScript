9f3c855ab8a449a7b4987fdfcf0c8737d37c63fa
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-05-20 11:42:16 -0500

    Definiendo e instanciando una clase

---
+++ Curso JS Moderno/24-POO/js/01-app.js
@@ -0,0 +1,17 @@
+class Cliente {
+  constructor(nombre, saldo) {
+    this.nombre = nombre;
+    this.saldo = saldo;
+  }
+}
+
+const juan = new Cliente("Juan", 400);
+console.log(juan);
+
+const Cliente2 = class {
+  constructor(nombre, saldo) {
+   
+  }
+};
+const juan2 = new Cliente2("Juan", 400);
+console.log(juan2);
