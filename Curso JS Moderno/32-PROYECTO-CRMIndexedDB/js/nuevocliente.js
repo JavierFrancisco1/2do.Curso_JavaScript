5e5af6f84cd85f33d0b59ba79de29be21bbfae13
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-06-17 18:00:47 -0500

    crm con indexedDB terminado

--- Curso JS Moderno/32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ Curso JS Moderno/32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -1,5 +1,5 @@
 (function () {
-  let DB;
+  
   const formulario = document.querySelector("#formulario");
 
   document.addEventListener("DOMContentLoaded", () => {
@@ -7,18 +7,6 @@
 
     formulario.addEventListener("submit", validarCliente);
   });
-
-  function conectarDB() {
-    const abrirConexion = window.indexedDB.open("crm", "1");
-
-    abrirConexion.onerror = function () {
-      console.log("Hubo un error");
-    };
-
-    abrirConexion.onsuccess = function () {
-      DB = abrirConexion.result;
-    };
-  }
 
   function validarCliente(e) {
     e.preventDefault();
@@ -55,59 +43,17 @@
     objectStore.add(cliente);
 
     transaction.onerror = function () {
-     imprimirAlerta("Hubo un error",'error');
+      imprimirAlerta("Hubo un error", "error");
     };
 
-    transaction.oncomplete = function(){
-      console.log('Cliente agregado');
+    transaction.oncomplete = function () {
+      console.log("Cliente agregado");
 
-      imprimirAlerta('El cliente se agrego correctamente');
-
-      setTimeout(() => {
-        window.location.href = 'index.html';
-      },3000)
-    }
-  }
-
-  function imprimirAlerta(mensaje, tipo) {
-    const alerta = document.querySelector(".alerta");
-
-    if (!alerta) {
-      // Crear la alerta
-      const divMensaje = document.createElement("div");
-      divMensaje.classList.add(
-        "px-4",
-        "px-3",
-        "rounded",
-        "max-w-lg",
-        "mx-auto",
-        "mt-6",
-        "text-center",
-        "border",
-        "alerta"
-      );
-
-      if (tipo === "error") {
-        divMensaje.classList.add(
-          "bg-red-100",
-          "border-red-400",
-          "text-red-700"
-        );
-      } else {
-        divMensaje.classList.add(
-          "bg-green-100",
-          "border-green-400",
-          "text-green-700"
-        );
-      }
-
-      divMensaje.textContent = mensaje;
-
-      formulario.appendChild(divMensaje);
+      imprimirAlerta("El cliente se agrego correctamente");
 
       setTimeout(() => {
-        divMensaje.remove();
+        window.location.href = "index.html";
       }, 3000);
-    }
+    };
   }
 })();
