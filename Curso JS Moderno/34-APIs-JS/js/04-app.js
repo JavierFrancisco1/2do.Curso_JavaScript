b57ea5cca10f5a261873b217b22958f42d024380
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-06-20 19:01:02 -0500

    requestFullScreen y exitFullScreen
    ejecutar pantalla completa

--- Curso JS Moderno/34-APIs-JS/js/04-app.js
+++ Curso JS Moderno/34-APIs-JS/js/04-app.js
@@ -1,1 +1,15 @@
-console.log('Hola mundo');
+const abrirBtn = document.querySelector('#abrir-pantalla-completa')
+const salirBtn = document.querySelector('#salir-pantalla-completa')
+
+
+abrirBtn.addEventListener('click',pantallaCompleta);
+salirBtn.addEventListener('click',CerrarPantallaCompleta);
+
+function pantallaCompleta(){
+    document.documentElement.requestFullscreen();
+}
+
+function CerrarPantallaCompleta(){
+    document.exitFullscreen();
+}
+
