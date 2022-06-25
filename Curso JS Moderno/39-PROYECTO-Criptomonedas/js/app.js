52b887b154e449cb430f314b504f2df222da14ad
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-06-24 19:12:25 -0500

    PROYECTO: cotizador de criptomonedas

--- Curso JS Moderno/39-PROYECTO-Criptomonedas/js/app.js
+++ Curso JS Moderno/39-PROYECTO-Criptomonedas/js/app.js
@@ -1,4 +1,12 @@
 const criptoMonedasSelect = document.querySelector("#criptomonedas");
+const monedaSelect = document.querySelector("#moneda");
+const formulario = document.querySelector("#formulario");
+const resultado = document.querySelector("#resultado");
+
+const objBusqueda = {
+  moneda: "",
+  criptomoneda: "",
+};
 
 //Crear Un Promise
 const obtenerCriptoMonedas = (criptomonedas) =>
@@ -8,6 +16,9 @@ const obtenerCriptoMonedas = (criptomonedas) =>
 
 document.addEventListener("DOMContentLoaded", () => {
   consultarCriptoMonedas();
+  formulario.addEventListener("submit", submitFormulario);
+  criptoMonedasSelect.addEventListener("change", leerValor);
+  monedaSelect.addEventListener("change", leerValor);
 });
 
 function consultarCriptoMonedas() {
@@ -29,4 +40,103 @@ function selectCriptomonedas(criptomonedas) {
     option.textContent = FullName;
     criptoMonedasSelect.appendChild(option);
   });
+}
+
+function leerValor(e) {
+  objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e) {
+  e.preventDefault();
+
+  // Validar el formulario
+  const { moneda, criptomoneda } = objBusqueda;
+
+  if (moneda === "" || criptomoneda === "") {
+    mostrarAlerta("Ambos campos son obligatorios");
+    return;
+  }
+
+  // Consultar la API con los resultados
+  consultarAPI();
+}
+
+function mostrarAlerta(mensaje) {
+  const existeError = document.querySelector(".error");
+
+  if (!existeError) {
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add("error");
+
+    // Mensaje de error
+    divMensaje.textContent = mensaje;
+    formulario.appendChild(divMensaje);
+
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+}
+
+function consultarAPI() {
+  const { moneda, criptomoneda } = objBusqueda;
+
+  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+  mostrarSpinner();
+
+  fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((cotizacion) => {
+      mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
+    });
+}
+
+function mostrarCotizacionHTML(cotizacion) {
+  limpiarHTML();
+
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+
+  const precio = document.createElement("p");
+  precio.classList.add("precio");
+  precio.innerHTML = `<small class='transparent'>El precio es:</small> <span class='p-short'>${PRICE}</span>`;
+
+  const precioAlto = document.createElement("p");
+  precioAlto.innerHTML = `<small class='transparent'>Precio mas alto del dia:</small> <span class='p-short'>${HIGHDAY}</span>`;
+
+  const precioBajo = document.createElement("p");
+  precioBajo.innerHTML = `<small class='transparent'>Precio mas bajo del dia:</small> <span class='p-short'>${LOWDAY}</span>`;
+
+  const ultimasHoras = document.createElement("p");
+  ultimasHoras.innerHTML = `<small class='transparent'>Variación ultimas 24 horas:</small> <span class='p-short'>${CHANGEPCT24HOUR}%</span>`;
+
+  const ultimaActualizacion = document.createElement("p");
+  ultimaActualizacion.innerHTML = `<small class='transparent'>Ultima actualización:</small> <span class='p-short'>${LASTUPDATE}</span>`;
+
+  resultado.appendChild(precio);
+  resultado.appendChild(precioAlto);
+  resultado.appendChild(precioBajo);
+  resultado.appendChild(ultimasHoras);
+  resultado.appendChild(ultimaActualizacion);
+}
+
+function limpiarHTML() {
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
+}
+
+function mostrarSpinner() {
+  limpiarHTML();
+
+  const spinner = document.createElement("div");
+  spinner.classList.add("spinner");
+
+  spinner.innerHTML = `
+    <div class="bounce1"></div>
+    <div class="bounce2"></div>
+    <div class="bounce3"></div>
+`;
+
+  resultado.appendChild(spinner);
 }
