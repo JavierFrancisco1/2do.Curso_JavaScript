8c781e4e24c197cec1a10a5a7d9847f711e94457
estebanfon <82420612+JavierFrancisco1@users.noreply.github.com>
2022-07-17 21:58:06 -0500

    envío de mail

--- Curso_JS_Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso_JS_Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,140 +1,280 @@
-//Variables
+// //Variables
+// const btnEnviar = document.querySelector("#enviar");
+// const btnReset = document.querySelector("#resetBtn");
+// const form = document.querySelector("#enviar-mail");
+
+// //Variables de los campos
+// const Email = document.querySelector("#email");
+// const Asunto = document.querySelector("#asunto");
+// const Mensaje = document.querySelector("#mensaje");
+
+// const exp =
+//   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// //eventos
+// EventListener();
+// function EventListener() {
+//   //cargando la App y el DOM
+//   document.addEventListener("DOMContentLoaded", iniciarApp);
+
+//   //eventos de los input
+//   Email.addEventListener("blur", validarFormulario);
+//   Asunto.addEventListener("blur", validarFormulario);
+//   Mensaje.addEventListener("blur", validarFormulario);
+
+//   btnReset.addEventListener("click", resetear);
+
+//   form.addEventListener("submit", enviarEmail);
+// }
+
+// //funciones
+
+// function iniciarApp() {
+//   btnEnviar.disabled = true;
+//   btnEnviar.classList.add('cursor-not-allowed')
+// }
+
+// function validarFormulario(e) {
+//   e.preventDefault();
+
+//   if (e.target.value.length > 0) {
+//     //eliminamos los errores
+
+//     e.target.classList.remove("border", "border-red-500");
+//     e.target.classList.add("border", "border-green-500");
+
+//     const errores = document.querySelector("p.error");
+
+//     if (errores) {
+//       errores.remove();
+//     }
+//   } else {
+//     e.target.classList.remove("border", "border-green-500");
+//     e.target.classList.add("border", "border-red-500");
+//     mostrarError("Todos los campos son obligatorios");
+//   }
+
+//   if (e.target.type === "email") {
+//     if (exp.test(e.target.value)) {
+//       e.target.classList.remove("border", "border-red-500");
+//       e.target.classList.add("border", "border-green-500");
+
+//       const errores = document.querySelector("p.error");
+
+//       if (errores) {
+//         errores.remove();
+//       }
+//     } else {
+//       e.target.classList.remove("border", "border-green-500");
+//       e.target.classList.add("border", "border-red-500");
+
+//       mostrarError("El email no es valido");
+//     }
+//   }
+
+//   if (exp.test(Email.value) && Asunto.value !== "" && Mensaje.value !== "") {
+//     btnEnviar.disabled = false;
+//     btnEnviar.classList.remove("cursor-not-allowed", "opacity-50");
+//   }
+// }
+
+// //vamos a mostrar los errores en validacion
+
+// function mostrarError(mensajito) {
+//   const mensajeError = document.createElement("p");
+//   mensajeError.textContent = mensajito;
+//   mensajeError.classList.add(
+//     "border",
+//     "border-red-500",
+//     "background-red-100",
+//     "text-red-500",
+//     "p-3",
+//     "mt-5",
+//     "text-center",
+//     "error"
+//   );
+
+//   const variosMensajesError = document.querySelectorAll(".error");
+
+//   if (variosMensajesError.length === 0) {
+//     form.appendChild(mensajeError);
+//   }
+// }
+
+// function enviarEmail(e) {
+//   e.preventDefault();
+
+//   const spinner = document.querySelector("#spinner");
+//   spinner.style.display = "flex";
+
+//   // Después de 3 segundos ocultar el spinner
+//   setTimeout(() => {
+//     spinner.style.display = "none";
+
+//     const parrafo = document.createElement("p");
+//     parrafo.textContent = "el mensaje se envió correctamente";
+//     parrafo.classList.add(
+//       "my-10",
+//       "text-center",
+//       "bg-green-500",
+//       "p-2",
+//       "text-white",
+//       "font-bold",
+//       "uppercase"
+//     );
+
+//     form.insertBefore(parrafo, spinner);
+
+//     setTimeout(() => {
+//       parrafo.remove();
+
+//       resetear();
+//     }, 5000);
+//   }, 3000);
+// }
+
+// function resetear(e) {
+//   form.reset();
+//   Email.classList.remove("border-green-500", "border-red-500");
+//   Asunto.classList.remove("border-green-500", "border-red-500");
+//   Mensaje.classList.remove("border-green-500", "border-red-500");
+// }
+
+// Variables
 const btnEnviar = document.querySelector("#enviar");
 const btnReset = document.querySelector("#resetBtn");
-const form = document.querySelector("#enviar-mail");
+const formulario = document.querySelector("#enviar-mail");
 
-//Variables de los campos
-const Email = document.querySelector("#email");
-const Asunto = document.querySelector("#asunto");
-const Mensaje = document.querySelector("#mensaje");
-
-const exp =
+//Exprecion regular para email
+const er =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-//eventos
-EventListener();
-function EventListener() {
-  //cargando la App y el DOM
+// Variables para Campos
+const email = document.querySelector("#email");
+const asunto = document.querySelector("#asunto");
+const mensaje = document.querySelector("#mensaje");
+
+eventListeners();
+function eventListeners() {
+  //Cuando la app arranca
   document.addEventListener("DOMContentLoaded", iniciarApp);
 
-  //eventos de los input
-  Email.addEventListener("blur", validarFormulario);
-  Asunto.addEventListener("blur", validarFormulario);
-  Mensaje.addEventListener("blur", validarFormulario);
+  //Validar los campos del formulario
+  email.addEventListener("blur", validarFormulario);
+  asunto.addEventListener("blur", validarFormulario);
+  mensaje.addEventListener("blur", validarFormulario);
 
-  btnReset.addEventListener("click", resetear);
+  // Enviar Email
+  formulario.addEventListener("submit", enviarEmail);
 
-  form.addEventListener("submit", enviarEmail);
+  // Boton que reinicia el formulario
+  btnReset.addEventListener("click", resetearFormulario);
 }
 
-//funciones
-
+// Funciones
 function iniciarApp() {
   btnEnviar.disabled = true;
-  btnEnviar.classList.add('cursor-not-allowed')
+  btnEnviar.classList.add("cursor-not-allowed", "opacity-50");
 }
 
+//Valida el formulario
 function validarFormulario(e) {
-  e.preventDefault();
+  if (e.target.type === "email") {
+    if (er.test(email.value)) {
+      const error = document.querySelector("p.error");
+      if (error) {
+        error.remove();
+      }
+      e.target.classList.remove("border-red-500", "border");
+      e.target.classList.add("border-green-500", "border");
+    } else {
+      e.target.classList.remove("border-green-500", "border");
+      e.target.classList.add("border-red-500", "border");
 
-  if (e.target.value.length > 0) {
-    //eliminamos los errores
-
-    e.target.classList.remove("border", "border-red-500");
-    e.target.classList.add("border", "border-green-500");
-
-    const errores = document.querySelector("p.error");
-
-    if (errores) {
-      errores.remove();
+      mostrarError("Email no valido");
     }
+  }
+  if (e.target.value.length > 0) {
+    //Elimina los errores...
+    const error = document.querySelector("p.error");
+    if (error) {
+      error.remove();
+    }
+    e.target.classList.remove("border-red-500", "border");
+    e.target.classList.add("border-green-500", "border");
   } else {
-    e.target.classList.remove("border", "border-green-500");
-    e.target.classList.add("border", "border-red-500");
+    e.target.classList.remove("border-green-500", "border");
+    e.target.classList.add("border-red-500", "border");
+
     mostrarError("Todos los campos son obligatorios");
   }
 
-  if (e.target.type === "email") {
-    if (exp.test(e.target.value)) {
-      e.target.classList.remove("border", "border-red-500");
-      e.target.classList.add("border", "border-green-500");
-
-      const errores = document.querySelector("p.error");
-
-      if (errores) {
-        errores.remove();
-      }
-    } else {
-      e.target.classList.remove("border", "border-green-500");
-      e.target.classList.add("border", "border-red-500");
-
-      mostrarError("El email no es valido");
-    }
-  }
-
-  if (exp.test(Email.value) && Asunto.value !== "" && Mensaje.value !== "") {
+  if (er.test(email.value) && asunto.value !== "" && mensaje.value !== "") {
     btnEnviar.disabled = false;
     btnEnviar.classList.remove("cursor-not-allowed", "opacity-50");
   }
 }
 
-//vamos a mostrar los errores en validacion
-
-function mostrarError(mensajito) {
+function mostrarError(mensaje) {
   const mensajeError = document.createElement("p");
-  mensajeError.textContent = mensajito;
+  mensajeError.textContent = mensaje;
   mensajeError.classList.add(
-    "border",
     "border-red-500",
+    "border",
     "background-red-100",
     "text-red-500",
     "p-3",
-    "mt-5",
     "text-center",
+    "mt-5",
     "error"
   );
 
-  const variosMensajesError = document.querySelectorAll(".error");
+  const errores = document.querySelectorAll(".error");
 
-  if (variosMensajesError.length === 0) {
-    form.appendChild(mensajeError);
+  if (errores.length === 0) {
+    formulario.appendChild(mensajeError);
   }
 }
 
+// Envia el Email
 function enviarEmail(e) {
   e.preventDefault();
 
+  // Mostrar el spinner
   const spinner = document.querySelector("#spinner");
   spinner.style.display = "flex";
 
   setTimeout(() => {
     spinner.style.display = "none";
 
+    // Mensaje que dice que se envio correctamente
     const parrafo = document.createElement("p");
-    parrafo.textContent = "el mensaje se envió correctamente";
+    parrafo.textContent = "El mensaje se envió correctamente";
     parrafo.classList.add(
-      "my-10",
       "text-center",
-      "bg-green-500",
+      "my-10",
       "p-2",
+      "bg-green-500",
       "text-white",
       "font-bold",
       "uppercase"
     );
 
-    form.insertBefore(parrafo, spinner);
+    // Inserta el mensaje antes del spinner
+    formulario.insertBefore(parrafo, spinner);
 
     setTimeout(() => {
       parrafo.remove();
-
-      resetear();
+      resetearFormulario();
     }, 5000);
-  }, 3000);
+  }, 4000);
 }
 
-function resetear(e) {
-  form.reset();
-  Email.classList.remove("border-green-500", "border-red-500");
-  Asunto.classList.remove("border-green-500", "border-red-500");
-  Mensaje.classList.remove("border-green-500", "border-red-500");
+// Resetea los campos del formulario
+function resetearFormulario() {
+  formulario.reset();
+  iniciarApp();
+  email.classList.remove("border-green-500", "border-red-500");
+  asunto.classList.remove("border-green-500", "border-red-500");
+  mensaje.classList.remove("border-green-500", "border-red-500");
 }
--- Curso_JS_Moderno/17-PROYECTO-Buscador/js/app.js
+++ Curso_JS_Moderno/17-PROYECTO-Buscador/js/app.js
@@ -1,7 +1,206 @@
-const formulario = document.querySelector("#buscador");
-console.log(formulario);
+// const formulario = document.querySelector("#buscador");
+// console.log(formulario);
 
-//variables
+// //variables
+// const marca = document.querySelector("#marca");
+// const year = document.querySelector("#year");
+// const minimo = document.querySelector("#minimo");
+// const maximo = document.querySelector("#maximo");
+// const puertas = document.querySelector("#puertas");
+// const transmision = document.querySelector("#transmision");
+// const color = document.querySelector("#color");
+
+// // Contenedor para los resultados
+// const resultado = document.querySelector("#resultado");
+
+// const max = new Date().getFullYear() - 2;
+// const min = max - 10;
+
+// // Generar un objeto con la busqueda
+// const datosBusqueda = {
+//   marca: "",
+//   year: "",
+//   minimo: "",
+//   maximo: "",
+//   puertas: "",
+//   transmision: "",
+//   color: "",
+// };
+
+// //eventos
+// document.addEventListener("DOMContentLoaded", () => {
+//   mostrarAutos(autos); // Muestra los autos al cargar
+
+//   // Llena las opciones de años
+//   llenarSelect();
+// });
+
+// // Event listener para los select de busqueda
+// marca.addEventListener("change", (e) => {
+//   datosBusqueda.marca = e.target.value;
+
+//   filtrarAuto();
+// });
+// year.addEventListener("change", (e) => {
+//   datosBusqueda.year = parseInt(e.target.value);
+//   filtrarAuto();
+// });
+// minimo.addEventListener("change", (e) => {
+//   datosBusqueda.minimo = e.target.value;
+
+//   filtrarAuto();
+// });
+// maximo.addEventListener("change", (e) => {
+//   datosBusqueda.maximo = e.target.value;
+//   filtrarAuto();
+// });
+// puertas.addEventListener("change", (e) => {
+//   datosBusqueda.puertas = e.target.value;
+//   filtrarAuto();
+// });
+// transmision.addEventListener("change", (e) => {
+//   datosBusqueda.transmision = e.target.value;
+//   filtrarAuto();
+// });
+// color.addEventListener("change", (e) => {
+//   datosBusqueda.color = e.target.value;
+//   filtrarAuto();
+// });
+
+// // Funciones
+// function mostrarAutos(autos) {
+//   limpiarHTML(); // Elimina el HTML previo
+//   autos.forEach((auto) => {
+//     const { marca, modelo, year, puertas, transmision, precio, color } = auto;
+//     const autoHTML = document.createElement("p");
+
+//     autoHTML.textContent = `
+//          ${marca} ${modelo} - ${year} - ${puertas} puertas - transmisión: ${transmision} - precio: ${precio} - color: ${color}
+
+//         `;
+
+//     // Inserta en el HTML
+//     resultado.appendChild(autoHTML);
+//   });
+// }
+
+// // Limpiar HTML
+// function limpiarHTML() {
+//   while (resultado.firstChild) {
+//     resultado.removeChild(resultado.firstChild);
+//   }
+// }
+
+// // Genera los años del select
+// function llenarSelect() {
+//   for (let i = max; i >= min; i--) {
+//     const opcion = document.createElement("option");
+//     opcion.value = i;
+//     opcion.textContent = i;
+//     year.appendChild(opcion); // Agrega las opciones del año
+//   }
+// }
+
+// //Funcion que filtra en base a la busqueda
+// function filtrarAuto() {
+//   const resultado = autos
+//     .filter(filtrarMarca)
+//     .filter(filtrarYear)
+//     .filter(filtrarMinimo)
+//     .filter(filtrarMaximo)
+//     .filter(filtrarPuertas)
+//     .filter(filtarTransmision)
+//     .filter(filtrarColor);
+//   if (resultado.length) {
+//     mostrarAutos(resultado);
+//   } else {
+//     noResultado();
+//   }
+// }
+
+// function noResultado() {
+//   limpiarHTML();
+//   const noResultado = document.createElement("div");
+//   noResultado.classList.add("alerta", "error");
+//   noResultado.textContent =
+//     "No Hay Resultados,Intenta con otros términos de búsqueda";
+//   resultado.appendChild(noResultado);
+//   setTimeout(() => {
+//     resultado.removeChild(noResultado);
+//     setTimeout(() => {
+//       mostrarAutos(autos);
+//       resetearFormulario();
+//     }, 1000);
+//   }, 5000);
+// }
+
+// function filtrarMarca(auto) {
+//   const { marca } = datosBusqueda;
+//   if (marca) {
+//     return auto.marca === marca;
+//   }
+//   return auto;
+// }
+
+// function filtrarYear(auto) {
+//   const { year } = datosBusqueda;
+//   if (year) {
+//     return auto.year === year;
+//   }
+//   return auto;
+// }
+
+// function filtrarMinimo(auto) {
+//   const { minimo } = datosBusqueda;
+//   if (minimo) {
+//     return auto.precio >= minimo;
+//   }
+//   return auto;
+// }
+
+// function filtrarMaximo(auto) {
+//   const { maximo } = datosBusqueda;
+//   if (maximo) {
+//     return auto.precio <= maximo;
+//   }
+//   return auto;
+// }
+
+// function filtrarPuertas(auto) {
+//   const { puertas } = datosBusqueda;
+//   if (puertas) {
+//     return auto.puertas === parseInt(puertas);
+//   }
+//   return auto;
+// }
+
+// function filtarTransmision(auto) {
+//   const { transmision } = datosBusqueda;
+//   if (transmision) {
+//     return auto.transmision === transmision;
+//   }
+//   return auto;
+// }
+
+// function filtrarColor(auto) {
+//   const { color } = datosBusqueda;
+//   if (color) {
+//     return auto.color === color;
+//   }
+//   return auto;
+// }
+
+// // funcion para resetar formulario cuando no se encontro algun resultado
+
+// function resetearFormulario() {
+//   formulario.reset();
+// }
+
+// Prueba ----------------- *************""""""
+
+// Variables ************
+const formulario = document.querySelector("#buscador");
+
 const marca = document.querySelector("#marca");
 const year = document.querySelector("#year");
 const minimo = document.querySelector("#minimo");
@@ -10,14 +209,13 @@ const puertas = document.querySelector("#puertas");
 const transmision = document.querySelector("#transmision");
 const color = document.querySelector("#color");
 
-// Contenedor para los resultados
-const resultado = document.querySelector("#resultado");
+// CONTENEDOR para los resultados
+const resultados = document.querySelector("#resultado");
 
 const max = new Date().getFullYear() - 2;
 const min = max - 10;
 
-// Generar un objeto con la busqueda
-const datosBusqueda = {
+const objetoInfo = {
   marca: "",
   year: "",
   minimo: "",
@@ -27,81 +225,73 @@ const datosBusqueda = {
   color: "",
 };
 
-//eventos
-document.addEventListener("DOMContentLoaded", () => {
-  mostrarAutos(autos); // Muestra los autos al cargar
+// *********** EVENTOS *************
 
-  // Llena las opciones de años
+document.addEventListener("DOMContentLoaded", () => {
+  // LLENA el select de año
   llenarSelect();
+
+  // MostrarAutos
+  mostrarAutos(autos);
 });
 
-// Event listener para los select de busqueda
+// Eventos para cada select y el filtrado..
 marca.addEventListener("change", (e) => {
-  datosBusqueda.marca = e.target.value;
-
+  objetoInfo.marca = e.target.value;
   filtrarAuto();
 });
 year.addEventListener("change", (e) => {
-  datosBusqueda.year = parseInt(e.target.value);
+  objetoInfo.year = parseInt(e.target.value);
   filtrarAuto();
 });
 minimo.addEventListener("change", (e) => {
-  datosBusqueda.minimo = e.target.value;
-
+  objetoInfo.minimo = e.target.value;
   filtrarAuto();
 });
 maximo.addEventListener("change", (e) => {
-  datosBusqueda.maximo = e.target.value;
+  objetoInfo.maximo = e.target.value;
   filtrarAuto();
 });
 puertas.addEventListener("change", (e) => {
-  datosBusqueda.puertas = e.target.value;
+  objetoInfo.puertas = parseInt(e.target.value);
   filtrarAuto();
 });
 transmision.addEventListener("change", (e) => {
-  datosBusqueda.transmision = e.target.value;
+  objetoInfo.transmision = e.target.value;
   filtrarAuto();
 });
 color.addEventListener("change", (e) => {
-  datosBusqueda.color = e.target.value;
+  objetoInfo.color = e.target.value;
   filtrarAuto();
 });
 
-// Funciones
+// ***** FUNCIONES ************
+
+function llenarSelect() {
+  for (let i = max; i >= min; i--) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = i;
+    year.appendChild(option);
+  }
+}
+
 function mostrarAutos(autos) {
-  limpiarHTML(); // Elimina el HTML previo
+  // Limpiar el HTML previo
+  limpiarHTML();
+
   autos.forEach((auto) => {
     const { marca, modelo, year, puertas, transmision, precio, color } = auto;
     const autoHTML = document.createElement("p");
 
     autoHTML.textContent = `
-         ${marca} ${modelo} - ${year} - ${puertas} puertas - transmisión: ${transmision} - precio: ${precio} - color: ${color}
-        
-        `;
-
-    // Inserta en el HTML
-    resultado.appendChild(autoHTML);
+             ${marca} ${modelo} - ${year} - ${puertas} puertas - transmisión: ${transmision} - precio: ${precio} - color: ${color}
+    
+            `;
+    resultados.appendChild(autoHTML);
   });
 }
 
-// Limpiar HTML
-function limpiarHTML() {
-  while (resultado.firstChild) {
-    resultado.removeChild(resultado.firstChild);
-  }
-}
-
-// Genera los años del select
-function llenarSelect() {
-  for (let i = max; i >= min; i--) {
-    const opcion = document.createElement("option");
-    opcion.value = i;
-    opcion.textContent = i;
-    year.appendChild(opcion); // Agrega las opciones del año
-  }
-}
-
-//Funcion que filtra en base a la busqueda
 function filtrarAuto() {
   const resultado = autos
     .filter(filtrarMarca)
@@ -109,8 +299,9 @@ function filtrarAuto() {
     .filter(filtrarMinimo)
     .filter(filtrarMaximo)
     .filter(filtrarPuertas)
-    .filter(filtarTransmision)
+    .filter(filtrarTransmision)
     .filter(filtrarColor);
+
   if (resultado.length) {
     mostrarAutos(resultado);
   } else {
@@ -118,24 +309,30 @@ function filtrarAuto() {
   }
 }
 
+// Funcion cuando no se encontraron resultados ***
 function noResultado() {
   limpiarHTML();
   const noResultado = document.createElement("div");
   noResultado.classList.add("alerta", "error");
   noResultado.textContent =
     "No Hay Resultados,Intenta con otros términos de búsqueda";
-  resultado.appendChild(noResultado);
-  setTimeout(() => {
-    resultado.removeChild(noResultado);
+  resultados.appendChild(noResultado);
+  if (resultados.firstChild) {
     setTimeout(() => {
-      mostrarAutos(autos);
-      resetearFormulario();
-    }, 1000);
-  }, 5000);
+      resultados.removeChild(noResultado);
+      setTimeout(() => {
+        mostrarAutos(autos);
+        resetearFormulario();
+      }, 1000);
+    }, 5000);
+  }
 }
 
+// Filtrar los  options
+
 function filtrarMarca(auto) {
-  const { marca } = datosBusqueda;
+  limpiarHTML();
+  const { marca } = objetoInfo;
   if (marca) {
     return auto.marca === marca;
   }
@@ -143,54 +340,59 @@ function filtrarMarca(auto) {
 }
 
 function filtrarYear(auto) {
-  const { year } = datosBusqueda;
+  limpiarHTML();
+  const { year } = objetoInfo;
   if (year) {
     return auto.year === year;
   }
   return auto;
 }
-
 function filtrarMinimo(auto) {
-  const { minimo } = datosBusqueda;
+  limpiarHTML();
+  const { minimo } = objetoInfo;
   if (minimo) {
     return auto.precio >= minimo;
   }
   return auto;
 }
-
 function filtrarMaximo(auto) {
-  const { maximo } = datosBusqueda;
+  limpiarHTML();
+  const { maximo } = objetoInfo;
   if (maximo) {
     return auto.precio <= maximo;
   }
   return auto;
 }
-
 function filtrarPuertas(auto) {
-  const { puertas } = datosBusqueda;
+  limpiarHTML();
+  const { puertas } = objetoInfo;
   if (puertas) {
-    return auto.puertas === parseInt(puertas);
+    return auto.puertas === puertas;
   }
   return auto;
 }
-
-function filtarTransmision(auto) {
-  const { transmision } = datosBusqueda;
+function filtrarTransmision(auto) {
+  limpiarHTML();
+  const { transmision } = objetoInfo;
   if (transmision) {
     return auto.transmision === transmision;
   }
   return auto;
 }
-
 function filtrarColor(auto) {
-  const { color } = datosBusqueda;
+  limpiarHTML();
+  const { color } = objetoInfo;
   if (color) {
     return auto.color === color;
   }
   return auto;
 }
 
-// funcion para resetar formulario cuando no se encontro algun resultado
+function limpiarHTML() {
+  if (resultados.firstChild) {
+    resultados.removeChild(resultados.firstChild);
+  }
+}
 
 function resetearFormulario() {
   formulario.reset();
